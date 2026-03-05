04cb616f54330384fb7d7ebb0cf91270ed10dfed
Harish kumar A B <harishaagaram@gmail.com>
2026-03-05 23:07:27 +0530

    sum

---
+++ 4.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+class Operation
+{
+public:
+    virtual void calculate() = 0;
+};
+
+class Addition : public Operation
+{
+    int a,b,sum;
+
+public:
+    void calculate()
+    {
+        cout<<"Enter two numbers: ";
+        cin>>a>>b;
+
+        this->sum = this->a + this->b;
+
+        cout<<"Sum = "<<sum<<endl;
+    }
+};
+
+int main()
+{
+    Operation *ptr;
+    Addition obj;
+
+    ptr=&obj;
+
+    ptr->calculate();
+}
